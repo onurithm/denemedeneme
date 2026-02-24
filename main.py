5ec9df54dc7dcdc51e5961394678d5f18bdaefe4
Onur YILDIZ <113312815+onurithm@users.noreply.github.com>
2026-02-24 12:09:15 +0300

    Change server host to 0.0.0.0 and port to 8080

--- main.py
+++ main.py
@@ -367,4 +367,4 @@ async def serve_dashboard():
     return FileResponse("static/dashboard.html")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8090, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
